9d6c954614366f2d1e29aaf6f658bfccabc89b97
maolro <43545783+maolro@users.noreply.github.com>
2021-04-23 18:02:03 +0200

    m

---
+++ character/gear.js
@@ -0,0 +1,17 @@
+var listofgear = { 
+   "Armor":[
+       {name:"Cuero",def:1, penalty:0}
+   ],
+   "Weapons":[
+       {name:"Espada larga",damage:3,type:"Filos cortantes",hands:1,reach:5},
+       {name:"Hacha de batalla",damage:5,type:"Filos cortantes",hands:1,reach:5},
+       {name:"Hacha de mano",damage:4,type:"Filos cortantes",hands:1,reach:5},
+       {name:"Gran hacha",damage:6,type:"Filos cortantes",hands:2,reach:5},
+       {name:"Mandoble",damage:4,type:"Filos cortantes",hands:2,reach:5},
+       {name:"Espada corta",damage:2,type:"Filos perforantes",hands:1,reach:5},
+       {name:"Daga",damage:1,type:"Filos cortantes",hands:1,reach:5},
+       {name:"Espada ropera",damage:3,type:"Filos perforantes",hands:1,reach:5},
+       {name:"Lanza",damage:3,type:"Alabardas",hands:1,reach:10}
+
+   ]
+}
