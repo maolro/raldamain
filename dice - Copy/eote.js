75eab85132ad3e9eeaa75b8e1a47dfa6c946c86b
maolro <43545783+maolro@users.noreply.github.com>
2021-09-01 13:11:22 +0200

    w

---
+++ dice - Copy/eote.js
@@ -0,0 +1,68 @@
+﻿// this script is for rolling dice for FFG's Edge of the Empire Star Wars RPG
+
+
+
+function getUrlParameter(name) {
+	name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+	var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+	var results = regex.exec(location.search);
+	return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+
+function edgeRoll( dice ) { // where the rolling magic happens
+	var finalRoll = 0;
+	var finalRollString = "";
+
+	
+
+	for ( var i = 0; i < dice.length ; i++ ) {
+		var sideNumber;
+		switch ( dice.charAt( i ) ) {
+			case "4" : sideNumber = 4; break;
+			case "6" : sideNumber = 6; break;
+			case "8" : sideNumber = 8; break;
+			case "x" : sideNumber = 10; break;
+			case "y" : sideNumber = 12; break;
+			case "z" : sideNumber = 20; break;
+		}
+		var roll;
+		roll = Math.floor(Math.random()*sideNumber)+1;
+		finalRoll += roll; 
+		finalRollString += "d"+sideNumber+ "= "+ roll + " ";
+	}
+	finalRollString += " "+document.getElementById("modifier").value;
+	finalRoll += document.getElementById("modifier").value;
+
+	var request = new XMLHttpRequest();
+	request.open("POST", "https://discord.com/api/webhooks/" + getUrlParameter("hook"));
+
+	request.setRequestHeader('Content-type', 'application/json');
+
+	var fields = [];
+	fields.push({ name: "result", value: finalRoll, inline: true });
+
+	var params = {
+		embeds: [{ title: getUrlParameter("name") + " ha tirado", description: finalRollString, fields: fields }]
+	}
+
+	alert(JSON.stringify(params));
+
+	request.send(JSON.stringify(params));
+
+	return finalRollString + '<br />' + finalRoll;
+	}
+
+	function image(name) {
+		return "e/" + name + ".png";
+    }
+
+function reducePool( callingImage ) { // removes a die from the entry box and the pool graphic
+	$('#diceEntry').val( $('#diceEntry').val().replace( callingImage.attr("src").charAt( 2 ), '' ) );
+	callingImage.remove();
+}
+
+function addDie( dieType ) { // adds a die to both the entry box and the pool graphic
+	$('#diceEntry').val( $('#diceEntry').val() + dieType.charAt( 0 ) );
+	$('#dicePool').append( '<img src="e/' + dieType + '.png" onclick="reducePool($(this));" />' );
+}
