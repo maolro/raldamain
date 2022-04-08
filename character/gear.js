6bc23e6c6396fdf5cd1e40dfc692e6066e786a66
maolro <43545783+maolro@users.noreply.github.com>
2022-04-08 16:29:15 +0200

    l

--- character/gear.js
+++ character/gear.js
@@ -5,16 +5,17 @@ var listofgear = {
 	   "Pesada": { def:6, penalty:-4},
 	   "Completa": {def:8, penalty:-6}
    },
-   "weapons":[
-       {name:"Espada larga",damage:3,type:"Filos cortantes",hands:1,reach:5},
-       {name:"Hacha de batalla",damage:5,type:"Filos cortantes",hands:1,reach:5},
-       {name:"Hacha de mano",damage:4,type:"Filos cortantes",hands:1,reach:5},
-       {name:"Gran hacha",damage:6,type:"Filos cortantes",hands:2,reach:5},
-       {name:"Mandoble",damage:4,type:"Filos cortantes",hands:2,reach:5},
-       {name:"Espada corta",damage:2,type:"Filos perforantes",hands:1,reach:5},
-       {name:"Daga",damage:1,type:"Filos cortantes",hands:1,reach:5},
-       {name:"Espada ropera",damage:3,type:"Filos perforantes",hands:1,reach:5},
-       {name:"Lanza",damage:3,type:"Alabardas",hands:1,reach:10}
+   "weapons":{
+       "Espada larga": {damage:3,type:"Filos cortantes",hands:1,reach:5},
+       "Hacha de batalla": {damage:5,type:"Filos cortantes",hands:1,reach:5},
+       "Hacha de mano": {damage:4,type:"Filos cortantes",hands:1,reach:5},
+       "Gran hacha": {damage:6,type:"Filos cortantes",hands:2,reach:5},
+       "Mandoble": {damage:4,type:"Filos cortantes",hands:2,reach:5},
+       "Espada corta": {damage:2,type:"Filos perforantes",hands:1,reach:5},
+       "Daga": {damage:1,type:"Filos cortantes",hands:1,reach:5},
+       "Espada ropera": {damage:3,type:"Filos perforantes",hands:1,reach:5},
+       "Lanza": {damage:3,type:"Alabardas",hands:2,reach:10},
+	   "Golpe": {damage:1,type:"Artes marciales",hands:1,reach:5},
 
-   ]
+   }
 }
