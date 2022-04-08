cb1a09081b9a082651f0da75e46562ecf3ed18bc
maolro <43545783+maolro@users.noreply.github.com>
2022-04-08 16:17:42 +0200

    i

--- character/gear.js
+++ character/gear.js
@@ -1,11 +1,11 @@
 var listofgear = { 
-   "Armor":[
-       {name:"Ligera",def:2, penalty:0},
-	   {name:"Media",def:4, penalty:-2},
-	   {name:"Pesada",def:6, penalty:-4},
-	   {name:"Completa",def:8, penalty:-6}
-   ],
-   "Weapons":[
+   "armor": {
+       "Ligera": { def:2, penalty:0},
+	   "Media": { def:4, penalty:-2},
+	   "Pesada": { def:6, penalty:-4},
+	   "Completa": {def:8, penalty:-6}
+   },
+   "weapons":[
        {name:"Espada larga",damage:3,type:"Filos cortantes",hands:1,reach:5},
        {name:"Hacha de batalla",damage:5,type:"Filos cortantes",hands:1,reach:5},
        {name:"Hacha de mano",damage:4,type:"Filos cortantes",hands:1,reach:5},
