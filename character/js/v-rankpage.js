19a6a86f7a8962b56432e83d554272285b56c750
maolro <matiasoliverroomp@outlook.com>
2024-10-17 17:44:49 +0200

    df

---
+++ character/js/v-rankpage.js
@@ -0,0 +1,61 @@
+Vue.component('v-rankpage', {
+    template: `
+<div>
+    <div class="row my-2 justify-content-center">
+        <b class="mr-1">Puntos a distribuir:</b>{{ rkpoints }}
+    </div>
+    <div v-for="(value, index) in myranks" :key="index">
+        <v-rank-selecter v-bind:ranks="ranks" :index="index" :limit="ranklimit" :enableval="rkpoints"
+        @update-rank-level="handleRankLevelUpdate" @remove-rank="removeRank"></v-rank-selecter>
+    </div>
+    <div class="row my-2 justify-content-center">
+        <button class="btn btn-secondary rounded-border px-3 py-1" @click="addRank">+</button>
+    </div>
+</div>
+`,
+    props: {
+        ranks: {
+            default: {},
+            type: Object,
+            required: true
+        },
+        rkpoints: {
+            type: Number
+        },
+        ranklimit: {
+            type: Number
+        }
+    },
+    computed: {
+        myranks() {
+            return this.$root.myranks;
+        }
+    },
+    methods: {
+        setRank: function (key, level, index) {
+            this.$set(this.myranks, index, { id: key, ...this.ranks[key] });
+            this.myranks[index].rank = level;
+        },
+        addRank: function () {
+            this.myranks.push({ rank: 0 });
+        },
+        removeRank: function () {
+            this.myranks.pop();
+        },
+        handleRankLevelUpdate({ key, level, index }) {
+            this.setRank(key, level, index);
+        },
+    },
+    watch: {
+        rkpoints: {
+            handler: function (newVal) {
+                this.rkpoints = newVal;
+            }
+        },
+        ranklimit: {
+            handler: function (newVal) {
+                this.ranklimit = newVal;
+            }
+        }
+    },
+});
