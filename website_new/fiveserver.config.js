0e50f3e89d61a41eb027320376b03f5f04fbfb0e
maolro <matiasoliverroomp@outlook.com>
2026-02-03 13:20:07 +0100

    redirect_fix_6

--- website_new/fiveserver.config.js
+++ website_new/fiveserver.config.js
@@ -7,17 +7,17 @@ module.exports = {
       if (req.url === '/reglas') {
         req.url = '/reglas.html';
       }
-      // 1. Handle "/rango/magia_fuego" -> serve "_rango.html"
+      // 1. Handle "/rango/magia_fuego" -> serve "view-rango.html"
       else if (req.url.startsWith('/rango/') && !req.url.includes('.')) {
-        req.url = '/_rango.html';
+        req.url = '/templates/view-rango.html';
       }
       // 2. Handle "/rangos" -> serve "rangos.html"
       else if (req.url === '/rangos') {
         req.url = '/rangos.html';
       }
-      // 3. Handle "/criatura/esqueleto" -> serve "criatura.html"
+      // 3. Handle "/criatura/esqueleto" -> serve "view-criatura.html"
       else if (req.url.startsWith('/criatura/') && !req.url.includes('.')) {
-        req.url = '/_criatura.html';
+        req.url = '/templates/view-criatura.html';
       }
       // 4. Handle "/criaturas" -> serve "criaturas.html"
       else if (req.url === '/criaturas') {
@@ -27,17 +27,17 @@ module.exports = {
       else if (req.url === '/mundo') {
         req.url = '/mundo.html';
       }
-      // 6. Handle "/mundo/categoria/naciones" -> serve "mundo_categoria.html"
+      // 6. Handle "/mundo/categoria/naciones" -> serve "view-mundo-categoria.html"
       else if (req.url.startsWith('/mundo/categoria/') && !req.url.includes('.')) {
-        req.url = '/_mundo_categoria.html';
+        req.url = '/templates/view-mundo-categoria.html';
       }
-      // 7. Handle "/mundo/articulo/feyn" -> serve "mundo_articulo.html"
+      // 7. Handle "/mundo/articulo/feyn" -> serve "view-mundo-articulo.html"
       else if (req.url.startsWith('/mundo/articulo/') && !req.url.includes('.')) {
-        req.url = '/_mundo_articulo.html';
+        req.url = '/templates/view-mundo-articulo.html';
       }
-      // 8. Handle "/equipo/espada_larga" -> serve "_equipo.html"
+      // 8. Handle "/equipo/espada_larga" -> serve "view-equipo.html"
       else if (req.url.startsWith('/equipo/') && !req.url.includes('.')) {
-        req.url = '/_equipo.html';
+        req.url = '/templates/view-equipo.html';
       }
       // 9. Handle "/equipos" -> serve "equipos.html"
       else if (req.url === '/equipos') {
