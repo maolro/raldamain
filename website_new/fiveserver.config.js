8f3b78352350a3d86ce2cf0efaf82b262abe3a0a
maolro <matiasoliverroomp@outlook.com>
2026-02-03 11:40:47 +0100

    url_fix

--- website_new/fiveserver.config.js
+++ website_new/fiveserver.config.js
@@ -5,7 +5,7 @@ module.exports = {
     (req, res, next) => {
       // 1. Handle "/rango/magia_fuego" -> serve "rango.html"
       if (req.url.startsWith('/rango/') && !req.url.includes('.')) {
-        req.url = '/rango.html';
+        req.url = '/_rango.html';
       }
       // 2. Handle "/rangos" -> serve "rangos.html"
       else if (req.url === '/rangos') {
@@ -13,7 +13,7 @@ module.exports = {
       }
       // 3. Handle "/criatura/esqueleto" -> serve "criatura.html"
       else if (req.url.startsWith('/criatura/') && !req.url.includes('.')) {
-        req.url = '/criatura.html';
+        req.url = '/_criatura.html';
       }
       // 4. Handle "/criaturas" -> serve "criaturas.html"
       else if (req.url === '/criaturas') {
@@ -25,15 +25,15 @@ module.exports = {
       }
       // 6. Handle "/mundo/categoria/naciones" -> serve "mundo_categoria.html"
       else if (req.url.startsWith('/mundo/categoria/') && !req.url.includes('.')) {
-        req.url = '/mundo_categoria.html';
+        req.url = '/_mundo_categoria.html';
       }
       // 7. Handle "/mundo/articulo/feyn" -> serve "mundo_articulo.html"
       else if (req.url.startsWith('/mundo/articulo/') && !req.url.includes('.')) {
-        req.url = '/mundo_articulo.html';
+        req.url = '/_mundo_articulo.html';
       }
       // 8. Handle "/criatura/esqueleto" -> serve "criatura.html"
       else if (req.url.startsWith('/equipo/') && !req.url.includes('.')) {
-        req.url = '/equipo.html';
+        req.url = '/_equipo.html';
       }
       next();
     }
